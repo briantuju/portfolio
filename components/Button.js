204bc492b1a24c5207db623e622ec8e30a7d9b94
Brian Tuju <briantuju@gmail.com>
2021-04-09 21:47:55 +0300

    Update contact page

--- components/Button.js
+++ components/Button.js
@@ -1,9 +1,15 @@
 /* eslint-disable react/prop-types */
+import { SpinnerInline } from "./Loading";
 
-export default function Button({ type = "button", children, ...props }) {
+export default function Button({
+  type = "button",
+  isLoading = false,
+  children,
+  ...props
+}) {
   return (
-    <button type={type} className="btn" {...props}>
-      {children}
+    <button type={type} className="btn" data-disabled={isLoading} {...props}>
+      {isLoading ? <SpinnerInline /> : children}
     </button>
   );
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,69 @@
+/* eslint-disable react/prop-types */
 import Head from "next/head";
+import { Form, Formik } from "formik";
+import schemas, { objSchema } from "../utils/schemas";
+import { fetcher } from "../lib/api";
+import TextAreaInput from "../components/formik/TextAreaInput";
+import TextInput from "../components/formik/TextInput";
+import Button from "../components/Button";
+
+const ContactForm = ({ handleSubmit }) => {
+  return (
+    <Formik
+      initialValues={{
+        name: "Brian",
+        email: "brian@mail.com",
+        comment: "Hello! This is a test message",
+      }}
+      validationSchema={objSchema({
+        email: schemas.email,
+        name: schemas.name,
+        comment: schemas.commentBody,
+      })}
+      onSubmit={async (values) => handleSubmit(values)}
+    >
+      {(formik) => (
+        <Form className="contact-form">
+          <TextInput label="Full Name" name="name" />
+
+          <TextInput label="Email" name="email" type="email" />
+
+          <TextAreaInput
+            label="Enter message"
+            placeholder="Type your message here"
+            name="comment"
+          />
+
+          <Button
+            type="submit"
+            isLoading={formik.isSubmitting}
+            disabled={!(formik.isValid && formik.dirty)}
+          >
+            Send message
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
 
 export default function Contact() {
+  const handleContactReq = async (values) => {
+    try {
+      const data = await fetcher("/api", values, "POST");
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <Head>
         <title>Contact</title>
       </Head>
-      <h1>Contact Page</h1>
+
+      <ContactForm handleSubmit={handleContactReq} />
     </div>
   );
 }
